1295db992e64f019728c10837879fd5e20a25ee1
baiwusanyu <740132583@qq.com>
2022-05-28 14:40:27 +0800

    feat: 实现nextTick

---
+++ example/nextTick/App.js
@@ -0,0 +1,42 @@
+import {h,ref} from '../../lib/be-vue.esm.js'
+export const App = {
+    // .vue
+    setup() {
+        const count = ref(0)
+        const handleClick = () =>{
+            for(let i = 0;i < 100; i++){
+                count.value++
+            }
+        }
+        return {
+            handleClick,
+            count
+        }
+    },
+    render() {
+        return h(
+            'div',
+            {
+                id: `hi be-vue${this.msg}`,
+                class: ['aaa', 'qwdqwdasssssss'],
+            },
+            [
+                h(
+                    'div',
+                    {
+                        id: `hi be-vue${this.msg}c`, class: ['ccc']
+                    },
+                    `${this.count}`
+                ),
+                h(
+                    'button',
+                    {
+                        id: `hi be-vue${this.msg}c`, class: ['ccc'],
+                        onClick:this.handleClick
+                    },
+                    `$click`
+                ),
+            ]
+        )
+    }
+}
---
+++ src/runtime-core/scheduler.ts
@@ -0,0 +1,28 @@
+let p = Promise.resolve()
+let queue = new Array()
+let isFlushPending = false
+
+export function nextTick(fn:any) {
+    return fn ? p.then(fn) : p
+}
+export function queueJobs(c:any) {
+    if(!queue.includes(queue)){
+        queue.push(queue)
+    }
+    
+}
+export function queueFlush() {
+    // 首次触发时，isFlushPending 设置为true
+    //  多次触发时，只添加到任务队列，然后到这里旧直接返回
+    // 任务执行完后再设置为false，避免多次调用 flushJobs
+    if(isFlushPending) return
+    isFlushPending = true
+    nextTick(flushJobs)
+}
+export function flushJobs(){
+    isFlushPending = false
+    let jobs
+    while ((jobs = queue.shift())){
+        jobs && jobs()
+    }
+}
